d78c49cc579762fc5839741a63706874ae4cffcb
jerem <jeremy.lardenois@hotmail.fr>
2020-12-15 12:13:23 +0100

    Récupère la liste de tous les pokémons ainsi que leur génération

--- recup_data.bash
+++ recup_data.bash
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-i=1
+id=1 # pokemon identiant of national pokedex
+gen=1 # number of the genertaion of the pokemon 
+idGen=1 # pokemon identifiant of the current generation
+pokemonByGen="151 100 135 107 156 72 88 89" # number of pokemons by generation
 
-while [ $i -le 20 ]
+for nbPokemons in $pokemonByGen
 do
-    name=`curl -Ls -o /dev/null -w %{url_effective} https://www.pokemon.com/us/pokedex/$i | cut -d'/' -f 6`
-    echo $i $name
-    ((i=i+1))
+    while [ $idGen -le $nbPokemons ]
+    do
+        name=`curl -Ls -o /dev/null -w %{url_effective} https://www.pokemon.com/us/pokedex/$id | cut -d'/' -f 6`;
+
+        echo $id $name $gen
+        ((id=id+1))
+        ((idGen+=1))
+    done
+    ((idGen=1))
+    ((gen+=1))
 done
